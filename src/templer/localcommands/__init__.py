dee8598c16cecf0ffa47998fca2722c1287021cd
cewing <cris@crisewing.com>
2012-05-12 22:24:05 -0700

    * add a package-level constant for the message to print when local commands are supported.  This package is a reasonable location for the default message to be shared by other packages which provide local commands

--- src/templer/localcommands/__init__.py
+++ src/templer/localcommands/__init__.py
@@ -3,4 +3,14 @@ Package API
 """
 from templer.localcommands.command import TemplerLocalCommand
 from templer.localcommands.template import TemplerLocalTemplate
+
+
 SUPPORTS_LOCAL_COMMANDS = True
+
+
+LOCAL_COMMANDS_MESSAGE = """
+Your new package supports local commands.  To access them, change
+directories into the 'src' directory inside your new package.  From
+there, you will be able to run the command `paster add --list` to see
+the local commands available for this package.
+"""
