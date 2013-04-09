7e84ccdb5eb157f1da617bb08771d5fba420e1bd
cewing <cris@crisewing.com>
2013-04-08 22:57:19 -0700

    * set up a mock local command template to run when testing  * patch mock command into the templer.localcommands distro at test setup, remove it at teardown.  * register the local command for the 'basic_namespace' template so we can test depending only on templer.core.  * rework 'available' test to use the new, locally available localcommand

--- src/templer/localcommands/tests/test_localcommand.py
+++ src/templer/localcommands/tests/test_localcommand.py
@@ -4,21 +4,32 @@ import sys
 import tempfile
 import unittest2 as unittest
 import StringIO
+import pkg_resources
 
 from templer.core.control_script import Runner
 from templer.core.tests.test_templates import read_sh
 from templer.core.tests.test_templates import templer as templercmd
 from templer.core.tests.test_templates import clean_working_set
+from templer.core.vars import var
+from templer.core.basic_namespace import BasicNamespace
+from templer.localcommands import TemplerLocalTemplate
 from templer.localcommands import TemplerLocalCommand
 
 
-def capture_output(command, *args, **kwargs):
-    oldout = sys.stdout
-    newout = StringIO.StringIO()
-    sys.stdout = newout
-    command(*args, **kwargs)
-    sys.stdout = oldout
-    return newout.getvalue()
+class ModuleLocalCommand(TemplerLocalTemplate):
+    """A bogus local command for use in testing"""
+    use_cheetah = True
+    parent_templates = ['basic_namespace', ]
+
+    _template_dir = 'templates/module'
+
+    vars = [
+      var('module_name', 'Module Name',  default="mymodule"), ]
+
+
+MOCK_EP = """
+module = templer.localcommands.tests.test_localcommand:ModuleLocalCommand
+"""
 
 
 class TestLocalCommands(unittest.TestCase):
@@ -29,18 +40,12 @@ class TestLocalCommands(unittest.TestCase):
         """
         self.orig_dir = os.getcwd()
         self.temp_dir = tempfile.mkdtemp()
-        # TODO: I am bothered by the fact that this depends on infrastructure
-        # built by the buildout for this package.  If the dev environment is
-        # different in some way, this is very brittle.  Is there a way to
-        # make the 'templer' shell script available to be run in a shell like
-        # we are using it below?
-        cmdpath = ['bin', 'templer']
-        cmdpath[:0] = self.orig_dir.split(os.path.sep)
-        self.cmd = os.path.sep.join(cmdpath)
+        self.dist = pkg_resources.get_distribution('templer.localcommands')
+        self._add_mock_localcommand(self.dist)
+        self.local_cmd = TemplerLocalCommand('MyCommand')
         self.options = [
-            'plone_basic', 'plone.example', '--no-interactive']
+            'basic_namespace', 'testing.example', '--no-interactive']
         os.chdir(self.temp_dir)
-        self.local_cmd = TemplerLocalCommand("My_Command")
 
     def tearDown(self):
         """cd back home, remove the temporary directory created for this test
@@ -49,6 +54,27 @@ class TestLocalCommands(unittest.TestCase):
         shutil.rmtree(self.temp_dir, ignore_errors=True)
         self.temp_dir = None
         clean_working_set()
+        self._remove_mock_localcommand(self.dist)
+
+    def _add_mock_localcommand(self, dist):
+        """set up a fake entry point for the duration of this test
+        """
+        self.old_ep_map = dist.get_entry_map()
+        new_ep_map = self.old_ep_map.copy()
+        new_ep_map['templer.templer_sub_template'] = {
+            'module': pkg_resources.EntryPoint.parse(MOCK_EP, dist=dist)
+        }
+        dist._ep_map = new_ep_map
+        # in addition, the basic namespace class should use_local_commands
+        BasicNamespace.use_local_commands = True
+
+    def _remove_mock_localcommand(self, dist):
+        """destroy the fake entry point created for this test
+        """
+        dist._ep_map = self.old_ep_map.copy()
+        del self.old_ep_map
+        # and finally, return BasicNamespace to original status
+        BasicNamespace.use_local_commands = False
 
     def test_add_list_option(self):
         """verify that the --list option works properly
@@ -57,7 +83,7 @@ class TestLocalCommands(unittest.TestCase):
         list the available local commands if some are.
         """
         self.fail('must write this test')
-
+    
     def test_add_list_all_option(self):
         """verify that the --list-all (-a) option works properly
         
@@ -65,32 +91,31 @@ class TestLocalCommands(unittest.TestCase):
         the current context.
         """
         self.fail('must write this test')
-
+    
     def test_add_no_locals_available(self):
         """verify that the add command fails gracefully when no localcommands
         are available in the current context
         """
         self.fail('must write this test')
-
+    
     def test_add_available_with_package(self):
         """verify that within a supporting package, add is available"""
-        # XXX we should create a local command just for testing,
-        # rather than assuming templer.plone.localcommands is present
         templercmd(" ".join(self.options))
-        os.chdir('plone.example')
+        os.chdir('testing.example')
         # we must re-initialize the runner so it can be context aware
         newrunner = Runner()
-        templercmd('add portlet --no-interactive', runner=newrunner)
-        home_path = os.path.join('src', 'plone', 'example')
-        self.assertTrue('portlets' in os.listdir(home_path))
-
+        templercmd('add module --no-interactive', runner=newrunner)
+        home_path = os.path.join('src', 'testing', 'example')
+        self.assertTrue('mymodule' in os.listdir(home_path))
+    
     def test_get_parent_namespace_packages(self):
         """verify that the current namespace package(s) can be determined"""
         templercmd(" ".join(self.options))
-        os.chdir(os.path.sep.join(['plone.example', 'src']))
+        os.chdir(os.path.sep.join(['testing.example', 'src']))
         # this should return a list of namespace packages
         namespaces = self.local_cmd.get_parent_namespace_packages()
-        expected = ('plone', '', 'example', 'plone.example/src/plone/example')
+        expected = ('testing', '', 'example',
+                    'testing.example/src/testing/example')
         self.assertEqual(len(namespaces), len(expected))
         for idx, node in enumerate(expected):
             found = namespaces[idx]
