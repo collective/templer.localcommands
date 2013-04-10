ba9488c7e7e8fd067c5495ae323ed6027c507b54
cewing <cris@crisewing.com>
2013-04-09 23:30:29 -0700

    continue fixing and extending test coverage

--- src/templer/localcommands/tests/test_localcommand.py
+++ src/templer/localcommands/tests/test_localcommand.py
@@ -14,6 +14,9 @@ from templer.core.vars import var
 from templer.core.basic_namespace import BasicNamespace
 from templer.localcommands import TemplerLocalTemplate
 from templer.localcommands import TemplerLocalCommand
+from templer.localcommands.command import NOT_LOCAL_CONTEXT_WARNING
+from templer.localcommands.command import TEMPLATE_NOT_SUPPORTED_WARNING
+from templer.localcommands.command import NO_TEMPLATE_SUPPORTED_WARNING
 
 
 class ModuleLocalCommand(TemplerLocalTemplate):
@@ -32,8 +35,8 @@ module = templer.localcommands.tests.test_localcommand:ModuleLocalCommand
 """
 
 
-class TestLocalCommands(unittest.TestCase):
-    """exercise the functions of the TemplerLocalCommand class"""
+class TestRunningLocalCommands(unittest.TestCase):
+    """Test run-time operation of localcommands"""
 
     def setUp(self):
         """create a temporary directory, cd to it for the duration of the test
@@ -42,7 +45,6 @@ class TestLocalCommands(unittest.TestCase):
         self.temp_dir = tempfile.mkdtemp()
         self.dist = pkg_resources.get_distribution('templer.localcommands')
         self._add_mock_localcommand(self.dist)
-        self.local_cmd = TemplerLocalCommand('MyCommand')
         self.options = [
             'basic_namespace', 'testing.example', '--no-interactive']
         os.chdir(self.temp_dir)
@@ -76,41 +78,82 @@ class TestLocalCommands(unittest.TestCase):
         # and finally, return BasicNamespace to original status
         BasicNamespace.use_local_commands = False
 
-    def test_add_list_option(self):
-        """verify that the --list option works properly
+    # def test_add_list_option_templates_available(self):
+    #     """verify that the --list option works properly
+    # 
+    #     It should print that no commands are available if none are, or 
+    #     list the available local commands if some are.
+    #     """
+    #     self.fail('must write this test')
+    # 
+    # def test_add_list_option_templates__not_available(self):
+    #     """verify that the --list option works properly
+    # 
+    #     It should print that no commands are available if none are, or 
+    #     list the available local commands if some are.
+    #     """
+    #     self.fail('must write this test')
+    # 
+    # def test_add_list_all_option(self):
+    #     """verify that the --list-all (-a) option works properly
+    #     
+    #     It should list all localcommands regardless of their availability in
+    #     the current context.
+    #     """
+    #     self.fail('must write this test')
 
-        It should print that no commands are available if none are, or 
-        list the available local commands if some are.
-        """
-        self.fail('must write this test')
-    
-    def test_add_list_all_option(self):
-        """verify that the --list-all (-a) option works properly
-        
-        It should list all localcommands regardless of their availability in
-        the current context.
-        """
-        self.fail('must write this test')
+    def test_add_not_in_context(self):
+        actual = templercmd("add module --no-interactive", silent=True)
+        expected = NOT_LOCAL_CONTEXT_WARNING
+        self.assertTrue(expected in actual)
     
     def test_add_no_locals_available(self):
         """verify that the add command fails gracefully when no localcommands
         are available in the current context
         """
-        self.fail('must write this test')
+        options = ['nested_namespace', 'testing.nested.example',
+                   '--no-interactive']
+        templercmd(" ".join(options), silent=True)
+        os.chdir('testing.nested.example')
+        actual = templercmd("add module --no-interactive", silent=True)
+        expected = NO_TEMPLATE_SUPPORTED_WARNING % 'nested_namespace'
+        self.assertTrue(expected in actual)
     
     def test_add_available_with_package(self):
         """verify that within a supporting package, add is available"""
-        templercmd(" ".join(self.options))
+        templercmd(" ".join(self.options), silent=True)
         os.chdir('testing.example')
         # we must re-initialize the runner so it can be context aware
         newrunner = Runner()
-        templercmd('add module --no-interactive', runner=newrunner)
+        templercmd('add module --no-interactive',
+                   runner=newrunner, silent=True)
         home_path = os.path.join('src', 'testing', 'example')
         self.assertTrue('mymodule' in os.listdir(home_path))
+
+
+class TestLocalCommandFunctions(unittest.TestCase):
+
+    def setUp(self):
+        """create a temporary directory, cd to it for the duration of the test
+        """
+        self.orig_dir = os.getcwd()
+        self.temp_dir = tempfile.mkdtemp()
+        self.local_cmd = TemplerLocalCommand('MyCommand')
+        self.options = [
+            'basic_namespace', 'testing.example', '--no-interactive']
+        os.chdir(self.temp_dir)
+
+    def tearDown(self):
+        """cd back home, remove the temporary directory created for this test
+        """
+        os.chdir(self.orig_dir)
+        shutil.rmtree(self.temp_dir, ignore_errors=True)
+        self.temp_dir = None
+        clean_working_set()
     
     def test_get_parent_namespace_packages(self):
         """verify that the current namespace package(s) can be determined"""
-        templercmd(" ".join(self.options))
+        templercmd(" ".join(self.options), silent=True)
         os.chdir(os.path.sep.join(['testing.example', 'src']))
         # this should return a list of namespace packages
         namespaces = self.local_cmd.get_parent_namespace_packages()
